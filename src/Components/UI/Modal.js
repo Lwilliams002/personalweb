1a798b4af7c932c6c1afc66326257c40edee4e84
Lesly Williams <lesly.williams001@mymdc.net>
2022-08-27 21:31:47 -0400

    bugs test

---
+++ src/Components/UI/Modal.js
@@ -0,0 +1,43 @@
+import {Button, Container, Form} from "react-bootstrap";
+import CustomPopup from "../../CustomPopup";
+import {useState} from "react";
+import "./Modal.css"
+
+function Modal() {
+    const [visibility, setVisibility] = useState(false);
+
+    const popupCloseHandler = (e) => {
+        setVisibility(e);
+    };
+    return (
+
+        <Container>
+            <Button onClick={(e) => setVisibility(!visibility)} className={"contact"}
+                    variant="outline-light">Contact</Button>
+            <CustomPopup
+                className={"contact-popup"}
+
+                onClose={popupCloseHandler}
+                show={visibility}
+                title="Let's get in touch!"
+            >
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Control type="text" placeholder="Name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control as="textarea" placeholder="Message" />
+                    </Form.Group>
+                    <Button className={"form-button"} variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </CustomPopup>
+        </Container>
+    )
+
+}
+export default Modal;
--- src/Components/UI/frontPage.js
+++ src/Components/UI/frontPage.js
@@ -1,23 +1,44 @@
 import "./frontPage.css";
 import {Col, Container} from "react-bootstrap";
 import {Row} from "react-bootstrap";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Typical from 'react-typical';
+import Modal from "./Modal";
+
 
 function frontPage() {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+
 
     return (
         <Container className={"frontPage"}>
             <Row>
-                <Col lg={12} >
+                <Col lg={12}>
                     <Container className={"justify-content-center"}>
-                        <img className={"profile"} src={"../EyeWinck.png"} alt={"Profile pic"} />
-                        <h1 className={"intro-title"}>Hi, I am<span className={"name-array"}> Lesly Williams</span></h1>
-                        <p className={"intro-sum"}>I am a frontend web developer. I can provide clean code and pixel perfect design. I also make website more & more interactive with web animations.</p>
-                        <div>
-                            <ul>
-                                <li><FontAwesomeIcon icon="fa-brands fa-github-square" /></li>
-                            </ul>
-                        </div>
+                        <Container>
+                            <img className={"profile"} src={"../EyeWinck.png"} alt={"Profile pic"}/>
+                            <h1 className={"intro-title"}>Hi, I am<h1 className={"intro-title-loop"}>
+                                <Typical
+                                    loop={Infinity}
+                                    wrapper="b"
+                                    steps={[
+                                        "Lesly Williams",
+                                        1000,
+                                        "Full Stack Developer",
+                                        1000,
+                                        "Project Manager",
+                                        1000,
+                                        "gamer",
+                                        1000
+                                    ]}
+                                />
+                            </h1>
+                            </h1>
+                            <p className={"intro-sum"}>I am a frontend web developer. I can provide clean code and pixel
+                                perfect design. I also make website more & more interactive with web animations.</p>
+                        </Container>
+                        <Container>
+                            <Modal/>
+                        </Container>
                     </Container>
                 </Col>
             </Row>
