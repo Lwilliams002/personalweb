226ff4dad7e6e7c806a5b378b5661c2477a6e4a1
Lesly Williams <lesly.williams001@mymdc.net>
2023-04-24 15:44:12 -0400

    updated project

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div>
         <MainPage />
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="personalweb/" element={<Home />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/projects" element={<Projects />} />
         </Routes>
